fe932a3806418c456d29f2567a6583519b93cf23
biruktadele <biruktadele55@gmail.com>
2025-10-03 13:57:40 +0300

    update

--- cp_manager/settings.py
+++ cp_manager/settings.py
@@ -179,7 +179,7 @@ else:
         'default': dj_database_url.config(
             default=os.environ.get("DATABASE_URL"),
             conn_max_age=600,
-            ssl_require=True,
+            # ssl_require=True,
         )
     }
 # DATABASES = {
